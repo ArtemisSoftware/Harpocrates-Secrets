7b403b77972ddf018ca02b2422286e9cb5bcee46
gustavo <gustavo.isaac@gmail.com>
2023-11-20 05:26:57 +0000

    Change MainActivity to AppCompatActivity to support Biometric authentication

--- app/src/main/java/com/artemissoftware/harpocratessecrets/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/harpocratessecrets/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.artemissoftware.harpocratessecrets
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import com.artemissoftware.harpocratessecrets.presentation.messages.MessageScreen
 import com.artemissoftware.harpocratessecrets.ui.theme.HarpocratesSecretsTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,9 +35,44 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    MessageScreen()
+                    val state = viewModel.state.collectAsState().value
+
+                    LaunchedEffect(Unit) {
+                        viewModel.onTriggerEvent(events = MainEvents.Authorize(this@MainActivity))
+                    }
+
+                    OnLifecycleEvent { _, event ->
+                        if (event == Lifecycle.Event.ON_PAUSE) {
+                            viewModel.onTriggerEvent(events = MainEvents.RevokeAuthorization)
+                        }
+                    }
+
+                    MessageScreen(
+                        authorized = state.authorized,
+                        biometricAuthentication = {
+                            viewModel.onTriggerEvent(events = MainEvents.Authorize(this@MainActivity))
+                        },
+                    )
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun OnLifecycleEvent(onEvent: (owner: LifecycleOwner, event: Lifecycle.Event) -> Unit) {
+    val eventHandler = rememberUpdatedState(newValue = onEvent)
+    val lifecycleOwner = rememberUpdatedState(newValue = LocalLifecycleOwner.current)
+
+    DisposableEffect(lifecycleOwner.value) {
+        val lifecycle = lifecycleOwner.value.lifecycle
+        val observer = LifecycleEventObserver { owner, event ->
+            eventHandler.value(owner, event)
+        }
+        lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycle.removeObserver(observer)
         }
     }
 }
